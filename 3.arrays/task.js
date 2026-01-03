6e9e2dc35350e347c262148a02d75f8ad96a0294
Igor Davydov <bzza@Igors-MacBook-Air.local>
2026-01-03 17:50:16 +0300

    3.arrays/task.js modified

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,25 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length === arr2.length) {
+        return arr1.every((element, index) => element === arr2[index]);
+    } else {
+        return false;
+    }
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    if (users.length === 0) {
+        return 0;
+    } else if (gender === "мужской" || gender === "женский") {
+        return users.filter(item => item.gender === gender).reduce((acc, item, idx, arr) => {
+            acc = acc + item.age;
+            if (idx === arr.length - 1) {
+                return acc / arr.length;
+            } else {
+                return acc;
+            }
+        }, 0);
+    } else {
+        return 0;
+    }
+
 }
