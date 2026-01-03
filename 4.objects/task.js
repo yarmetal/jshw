cf0ba4166043b5025aaee9d523beff3aa3f4d247
Igor Davydov <bzza@Igors-MacBook-Air.local>
2026-01-03 19:20:32 +0300

    4.objects/task.js modified

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,30 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+    if ('marks' in this === true) {
+        this.marks.push(...marksToAdd);
+    }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if ('marks' in this === false || this.marks.length === 0) {
+        return 0
+    }
+    return this.marks.reduce((acc, item) =>
+        acc += item, 0) / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
